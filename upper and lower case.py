feff3b5121cbdeec8723eaf2a21abb9f4d3a619c
parvj2006 <parvjivani@gmail.com>
2025-01-31 12:37:08 +0530

    Add files via upload

---
+++ upper and lower case.py
@@ -0,0 +1,11 @@
+# Uppercase Alphabets (A-Z)
+print("Uppercase Alphabets:")
+for i in range(65, 91):
+    print(chr(i), end=' ')
+print()
+
+# Lowercase Alphabets (a-z)
+print("Lowercase Alphabets:")
+for i in range(97, 123):
+    print(chr(i), end=' ')
+print()
